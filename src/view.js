ea7b85d5076428876298140cfb922798b2ccf72e
dimrux <dimrux@gmail.com>
2024-02-05 22:43:04 +0300

    refactoring view

--- src/view.js
+++ src/view.js
@@ -1,8 +1,125 @@
 /* eslint-disable curly */
 import onChange from 'on-change';
 
+const renderPosts = (state, i18n) => {
+  const posts = document.querySelector('.posts');
+  posts.textContent = '';
+  const divPosts = document.createElement('div');
+  divPosts.classList.add('card', 'border-0');
+  const divPostsHeader = document.createElement('div');
+  divPostsHeader.classList.add('card-body');
+  const divPostsHeaderTitle = document.createElement('h2');
+  divPostsHeaderTitle.classList.add('card-title', 'h4');
+  divPostsHeaderTitle.textContent = i18n.t('posts');
+  const ulPosts = document.createElement('ul');
+  ulPosts.classList.add('list-group', 'border-0', 'rounded-0');
+  state.posts.forEach((post) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-item-start', 'border-0', 'border-end-0');
+    const a = document.createElement('a');
+    const VisitedLinks = state.UIstate.modal;
+    if (VisitedLinks.includes(post.id)) {
+      a.classList.remove('fw-bold');
+      a.classList.add('fw-normal', 'link-secondary');
+    } else a.classList.add('fw-bold');
+    a.setAttribute('href', `${post.link}`);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.setAttribute('data-id', `${post.id}`);
+    a.textContent = post.content;
+    const btn = document.createElement('button');
+    btn.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('data-bs-toggle', 'modal');
+    btn.setAttribute('data-bs-target', '#modal');
+    btn.setAttribute('data-id', `${post.id}`);
+    btn.textContent = i18n.t('btnPostWatch');
+    li.append(a, btn);
+    ulPosts.append(li);
+  });
+  divPostsHeader.append(divPostsHeaderTitle);
+  divPosts.append(divPostsHeader, ulPosts);
+  posts.append(divPosts);
+};
+
+const renderFeeds = (state, i18n) => {
+  const feeds = document.querySelector('.feeds');
+  feeds.textContent = '';
+  const divFeeds = document.createElement('div');
+  divFeeds.classList.add('card', 'border-0');
+  const divFeedsHeader = document.createElement('div');
+  divFeedsHeader.classList.add('card-body');
+  const divFeedsHeaderTitle = document.createElement('h2');
+  divFeedsHeaderTitle.classList.add('card-title', 'h4');
+  divFeedsHeaderTitle.textContent = i18n.t('feeds');
+  const ulFeeds = document.createElement('ul');
+  ulFeeds.classList.add('list-group', 'border-0', 'rounded-0');
+  state.feeds.forEach((feed) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const titleFeed = document.createElement('h3');
+    titleFeed.classList.add('h6', 'm-0');
+    titleFeed.textContent = feed.title;
+    const descriptionFeed = document.createElement('p');
+    descriptionFeed.classList.add('m-0', 'small', 'text-black-50');
+    descriptionFeed.textContent = feed.descriptionRss;
+    li.append(titleFeed, descriptionFeed);
+    ulFeeds.append(li);
+  });
+  divFeedsHeader.append(divFeedsHeaderTitle);
+  divFeeds.append(divFeedsHeader, ulFeeds);
+  feeds.append(divFeeds);
+};
+
+const openModal = (state, modal, i18n, btn, ulPosts) => {
+  btn.addEventListener('click', () => {
+    const id = btn.getAttribute('data-id');
+    if (!state.UIstate.modal.includes(id)) {
+      state.UIstate.modal.push(id);
+      const a = ulPosts.querySelector(`[data-id="${id}"]`);
+      a.classList.remove('fw-bold');
+      a.classList.add('fw-normal', 'link-secondary');
+    }
+    const postByBtn = state.posts.filter((post) => post.id === id)[0];
+    const modalTitle = modal.querySelector('.modal-title');
+    modalTitle.textContent = postByBtn.content;
+    const modalBody = modal.querySelector('.modal-body');
+    modalBody.textContent = postByBtn.description;
+    const fullArticle = modal.querySelector('.full-article');
+    fullArticle.setAttribute('href', `${postByBtn.link}`);
+    fullArticle.textContent = i18n.t('fullArticle');
+    const btnCloseModal = modal.querySelector('.btn-secondary');
+    btnCloseModal.textContent = i18n.t('modalBtnClose');
+  });
+
+  const links = ulPosts.querySelectorAll('a');
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      const id = link.getAttribute('data-id');
+      if (!state.UIstate.modal.includes(id)) {
+        state.UIstate.modal.push(id);
+        link.classList.remove('fw-bold');
+        link.classList.add('fw-normal', 'link-secondary');
+      }
+    });
+  });
+};
+
+const closeModal = (button, modal$) => {
+  button.addEventListener('hidden.bs.modal', () => {
+    const nowModal = document.querySelector('.modal');
+    nowModal.textContent = '';
+    nowModal.append(modal$);
+  });
+};
+
 export default (elements, i18n, state) => onChange(state, (path, value) => {
-  const { sections, input, p } = elements;
+  const {
+    sections,
+    input,
+    p,
+    modal,
+  } = elements;
 
   const container = document.querySelector('.container-xxl');
   container.textContent = '';
@@ -18,109 +135,14 @@ export default (elements, i18n, state) => onChange(state, (path, value) => {
         p.textContent = i18n.t('successful');
       }
 
-      const posts = document.querySelector('.posts');
-      posts.textContent = '';
-      const divPosts = document.createElement('div');
-      divPosts.classList.add('card', 'border-0');
-      const divPostsHeader = document.createElement('div');
-      divPostsHeader.classList.add('card-body');
-      const divPostsHeaderTitle = document.createElement('h2');
-      divPostsHeaderTitle.classList.add('card-title', 'h4');
-      divPostsHeaderTitle.textContent = i18n.t('posts');
-      const ulPosts = document.createElement('ul');
-      ulPosts.classList.add('list-group', 'border-0', 'rounded-0');
-      state.posts.forEach((post) => {
-        const li = document.createElement('li');
-        li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-item-start', 'border-0', 'border-end-0');
-        const a = document.createElement('a');
-        const VisitedLinks = state.UIstate.modal;
-        if (VisitedLinks.includes(post.id)) {
-          a.classList.remove('fw-bold');
-          a.classList.add('fw-normal', 'link-secondary');
-        } else a.classList.add('fw-bold');
-        a.setAttribute('href', `${post.link}`);
-        a.setAttribute('target', '_blank');
-        a.setAttribute('rel', 'noopener noreferrer');
-        a.setAttribute('data-id', `${post.id}`);
-        a.textContent = post.content;
-        const btn = document.createElement('button');
-        btn.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-        btn.setAttribute('type', 'button');
-        btn.setAttribute('data-bs-toggle', 'modal');
-        btn.setAttribute('data-bs-target', '#modal');
-        btn.setAttribute('data-id', `${post.id}`);
-        btn.textContent = i18n.t('btnPostWatch');
-        li.append(a, btn);
-        ulPosts.append(li);
-      });
-      divPostsHeader.append(divPostsHeaderTitle);
-      divPosts.append(divPostsHeader, ulPosts);
-      posts.append(divPosts);
+      renderPosts(state, i18n);
+      renderFeeds(state, i18n);
 
-      const feeds = document.querySelector('.feeds');
-      feeds.textContent = '';
-      const divFeeds = document.createElement('div');
-      divFeeds.classList.add('card', 'border-0');
-      const divFeedsHeader = document.createElement('div');
-      divFeedsHeader.classList.add('card-body');
-      const divFeedsHeaderTitle = document.createElement('h2');
-      divFeedsHeaderTitle.classList.add('card-title', 'h4');
-      divFeedsHeaderTitle.textContent = i18n.t('feeds');
-      const ulFeeds = document.createElement('ul');
-      ulFeeds.classList.add('list-group', 'border-0', 'rounded-0');
-      state.feeds.forEach((feed) => {
-        const li = document.createElement('li');
-        li.classList.add('list-group-item', 'border-0', 'border-end-0');
-        const titleFeed = document.createElement('h3');
-        titleFeed.classList.add('h6', 'm-0');
-        titleFeed.textContent = feed.title;
-        const descriptionFeed = document.createElement('p');
-        descriptionFeed.classList.add('m-0', 'small', 'text-black-50');
-        descriptionFeed.textContent = feed.descriptionRss;
-        li.append(titleFeed, descriptionFeed);
-        ulFeeds.append(li);
-      });
-      divFeedsHeader.append(divFeedsHeaderTitle);
-      divFeeds.append(divFeedsHeader, ulFeeds);
-      feeds.append(divFeeds);
-
+      const ulPosts = document.querySelector('ul');
       const buttons = ulPosts.querySelectorAll('button');
       buttons.forEach((btn) => {
-        btn.addEventListener('click', () => {
-          const id = btn.getAttribute('data-id');
-          if (!state.UIstate.modal.includes(id)) {
-            state.UIstate.modal.push(id);
-            const a = ulPosts.querySelector(`[data-id="${id}"]`);
-            a.classList.remove('fw-bold');
-            a.classList.add('fw-normal', 'link-secondary');
-          }
-          const postByBtn = state.posts.filter((post) => post.id === id)[0];
-          const modalTitle = elements.modal.querySelector('.modal-title');
-          modalTitle.textContent = postByBtn.content;
-          const modalBody = elements.modal.querySelector('.modal-body');
-          modalBody.textContent = postByBtn.description;
-          const fullArticle = elements.modal.querySelector('.full-article');
-          fullArticle.setAttribute('href', `${postByBtn.link}`);
-          fullArticle.textContent = i18n.t('fullArticle');
-          const btnCloseModal = elements.modal.querySelector('.btn-secondary');
-          btnCloseModal.textContent = i18n.t('modalBtnClose');
-        });
-        btn.addEventListener('hidden.bs.modal', () => {
-          const nowModal = document.querySelector('.modal');
-          nowModal.textContent = '';
-          nowModal.append(elements.modal);
-        });
-        const links = ulPosts.querySelectorAll('a');
-        links.forEach((link) => {
-          link.addEventListener('click', () => {
-            const id = link.getAttribute('data-id');
-            if (!state.UIstate.modal.includes(id)) {
-              state.UIstate.modal.push(id);
-              link.classList.remove('fw-bold');
-              link.classList.add('fw-normal', 'link-secondary');
-            }
-          });
-        });
+        openModal(state, modal, i18n, btn, ulPosts);
+        closeModal(btn, modal);
       });
     }
   }
@@ -133,26 +155,5 @@ export default (elements, i18n, state) => onChange(state, (path, value) => {
     input.classList.add('is-invalid');
     p.classList.add('text-danger');
     p.textContent = i18n.t(state.error);
-  }
-
-  if (path === 'UIstate.modal') {
-    const postByBtn = state.posts.filter((post) => post.id === value.id)[0];
-    const body = document.querySelector('body');
-    body.classList.add('modal-open');
-    body.setAttribute('style', 'overflow: hidden; padding-right: 0px;');
-    elements.modal.classList.add('show');
-    elements.modal.setAttribute('style', 'display: block');
-    elements.modal.removeAttribute('aria-hidden');
-    elements.modal.setAttribute('aria-modal', 'true');
-    elements.modal.setAttribute('role', 'dialog');
-    const modalTitle = elements.modal.querySelector('.modal-title');
-    modalTitle.textContent = postByBtn.content;
-    const modalBody = elements.modal.querySelector('.modal-body');
-    modalBody.textContent = postByBtn.description;
-    const fullArticle = elements.modal.querySelector('.full-article');
-    fullArticle.setAttribute('href', `${postByBtn.link}`);
-    fullArticle.textContent = i18n.t('fullArticle');
-    const btnCloseModal = elements.modal.querySelector('.btn-secondary');
-    btnCloseModal.textContent = i18n.t('modalBtnClose');
   }
 });
